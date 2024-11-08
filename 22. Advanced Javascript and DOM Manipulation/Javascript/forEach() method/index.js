7585ad4a0d968595b9b5a9db18a357652ff40613
Sumit-531 <sumitchakraborty218@gmail.com>
2024-11-08 17:39:08 +0600

    Add forEach() method

---
+++ 22. Advanced Javascript and DOM Manipulation/Javascript/forEach() method/index.js
@@ -0,0 +1,47 @@
+//The forEach() method calls a function for each element in an array.
+//The forEach() method is not executed for empty elements.
+// array.forEach(callback)
+
+//=========================================================================
+
+// //Example 1:
+
+// let numbers = [1, 2, 3, 4, 5];
+
+// const display = (element) => console.log(element);
+
+// numbers.forEach(display);
+
+// //Example 2:
+// let numbers2 = [1, 2, 3, 4, 5];
+
+// function double(element, index, array){
+//     array[index] = element * 2;
+// }
+
+// const display2 = (element) => console.log(element);
+
+// numbers.forEach(double);
+// numbers.forEach(display2);
+
+//Example 3:
+// numbers3 = [1, 2, 4, 9];
+
+// const square = (element, index, array) => {
+//     array[index] = Math.pow(element, 2);
+// }
+// const display3 = (element) => console.log(element);
+
+// numbers3.forEach(square);
+// numbers3.forEach(display3);
+
+//Example 4:
+fruits = ["Mango", "Banana", "Licchi", "Jackfuit"];
+
+const upperCaseFruits = (element, index, array) => {
+    array[index] = element.toUpperCase();
+}
+const display4 = (element) => console.log(element);
+
+fruits.forEach(upperCaseFruits);
+fruits.forEach(display4);
