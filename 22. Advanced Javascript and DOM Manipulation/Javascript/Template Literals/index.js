eaa5665f0d21fc0b35b4a30f42033492ae82a63b
Sumit-531 <sumitchakraborty218@gmail.com>
2024-11-23 16:48:28 +0600

    Add Template literals

---
+++ 22. Advanced Javascript and DOM Manipulation/Javascript/Template Literals/index.js
@@ -0,0 +1,20 @@
+// Template literals are literals delimited with backtick (`) characters, 
+// allowing for multi-line strings, string interpolation with embedded expressions, 
+// and special constructs called tagged templates.
+
+//======================================================================================================
+
+//Example 1:
+let num1 = 4;
+let num2 = 9;
+
+result = num1 + num2;
+
+console.log(`The addition of ${num1} and ${num2} is ${result}`);
+
+//Example 2:
+console.log("The best way to predict the future is to invent it. \n- Alan Kay");
+
+//using Template Literal
+console.log(`The best way to predict the future is to invent it.
+   - Alan Kay`);
