c4b3125a00d1d5e2571316d8965174654d82c5fc
Sumit-531 <sumitchakraborty218@gmail.com>
2024-11-26 02:30:04 +0600

    Add Node.js

---
+++ 26. Node.js/1. Using node.js/calc.js
@@ -0,0 +1,8 @@
+let a = 3;
+let b = 9;
+
+function multiplication(firstNum, secondNum){
+    return firstNum * secondNum;
+}
+
+console.log(multiplication(a,b));
---
+++ 26. Node.js/3. The NPM Package Manager and Installing External Node Modules/2.3+NPM/2.3 NPM/index.js
@@ -0,0 +1,14 @@
+// const names = require("silly-names-hun");
+// import names from "silly-names-hun";
+
+
+// names.getRandomName(true)
+
+// console.log("My name is " + names);
+// console.log(names);
+
+import superheroes from 'superheroes';
+// const superheroes = require("superheroes");
+var name = superheroes;
+
+console.log("My name is "+name[3]+"!");
