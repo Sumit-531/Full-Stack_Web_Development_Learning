23af0b334d6ee101bf7e832ddd93483579011085
Sumit-531 <sumitchakraborty218@gmail.com>
2024-11-28 23:42:00 +0600

    Complete The Document Object Model(DOM)

---
+++ 20. The Document Object Model(DOM)/01. Adding Javascript to the Website/DOM/index.js
@@ -0,0 +1,8 @@
+alert("This is an external javascript.");
+
+var firstNum = Number(prompt("Write the first number: "));
+var secondNum = Number(prompt("Write the second number: "));
+
+sum = firstNum + secondNum;
+
+alert("The sum is "+sum);
