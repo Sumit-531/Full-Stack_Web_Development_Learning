fb16155b2349e3d0cce7cf000470103448940595
Sumit-531 <sumitchakraborty218@gmail.com>
2025-11-19 23:00:48 +0600

    add array destructuring

--- 22. Advanced Javascript and DOM Manipulation/Javascript/Destructuring/index.js
+++ 22. Advanced Javascript and DOM Manipulation/Javascript/Destructuring/index.js
@@ -6,7 +6,8 @@
 //**************************************************************************************************************
 // ARRAY DESTRUCTURING
 
-// Example 1: Swap The Values of Two Variables
+//****Swap The Values of Two Variables****/
+// Example 1:
 
 let a = 1;
 let b = 2;
@@ -15,5 +16,27 @@ let b = 2;
 // Right side -> [b, a] -> Creating array
 [a, b] = [b, a];
 
-console.log(a);
-console.log(b);
+console.log(a); // 2
+console.log(b); // 1
+
+//****Swap Two Elements in An Array */
+// Example 2:
+const districts = ["Dhaka", "Chittagong", "Sylhet", "Mymensingh", "Barishal"];
+
+// Dhaka, Barishal        Dhaka      Barishal
+// Array Destructuring
+[districts[0], districts[4]] = [districts[4], districts[0]]; // [ 'Barishal', 'Chittagong', 'Sylhet', 'Mymensingh', 'Dhaka' ]
+
+console.log(districts);
+
+//****Assign Array Elements to Variables */
+// Example 2:
+const colors = ["red", "green", "black", "yellow", "orange"];
+
+// Array destructuring
+const [firstColor, secondColor, thirdColor, ...extraColors] = colors;
+
+console.log(firstColor);
+console.log(secondColor);
+console.log(thirdColor);
+console.log(extraColors);
