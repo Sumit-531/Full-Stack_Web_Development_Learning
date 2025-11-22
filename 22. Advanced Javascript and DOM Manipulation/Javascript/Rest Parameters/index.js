9091d84f446e0457d57bca3611ad77edf8d5789d
Sumit-531 <sumitchakraborty218@gmail.com>
2025-11-22 21:10:53 +0600

    add rest parameters

---
+++ 22. Advanced Javascript and DOM Manipulation/Javascript/Rest Parameters/index.js
@@ -0,0 +1,4 @@
+// rest parameter is a parameter prefixed with three dots
+// rest parameters = (...rest) allow a function work with a variable number of arguments by building them into an array
+// spread = expands an array into seperate elements
+// rest = bundles separate elements into an array
