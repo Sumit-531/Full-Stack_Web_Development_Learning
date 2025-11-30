0115e0b65629af5b23718e4ef93c180ded6284ff
Sumit-531 <sumitchakraborty218@gmail.com>
2025-11-30 22:05:38 +0600

    add rest parameters example one

--- 22. Advanced Javascript and DOM Manipulation/Javascript/Rest Parameters/index.js
+++ 22. Advanced Javascript and DOM Manipulation/Javascript/Rest Parameters/index.js
@@ -2,3 +2,21 @@
 // rest parameters = (...rest) allow a function work with a variable number of arguments by building them into an array
 // spread = expands an array into seperate elements
 // rest = bundles separate elements into an array
+
+//**************************************************************************************************************
+// Example 1
+
+//               //Rest parameters
+function openFridge(...foods) {
+  console.log(foods);
+  //        // Spread Operator
+  console.log(...foods);
+}
+
+const foodOne = "Chicken Biriyani";
+const foodTwo = "Kacchi";
+const foodThree = "Khichuri";
+const foodFour = "Pizza";
+const foodFive = "Fried Rice";
+
+openFridge(foodOne, foodFour);
