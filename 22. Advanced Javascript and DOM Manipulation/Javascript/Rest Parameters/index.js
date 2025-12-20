d82ae6f9c911adb2d8cdd8bf4af5d6e9a9431057
Sumit-531 <sumitchakraborty218@gmail.com>
2025-12-20 23:49:24 +0600

    update rest parameters example one

--- 22. Advanced Javascript and DOM Manipulation/Javascript/Rest Parameters/index.js
+++ 22. Advanced Javascript and DOM Manipulation/Javascript/Rest Parameters/index.js
@@ -13,10 +13,22 @@ function openFridge(...foods) {
   console.log(...foods);
 }
 
+// use rest parameters -> stick all elements in array
+function getFood(...foods) {
+  return foods;
+}
+
 const foodOne = "Chicken Biriyani";
 const foodTwo = "Kacchi";
 const foodThree = "Khichuri";
 const foodFour = "Pizza";
 const foodFive = "Fried Rice";
 
-openFridge(foodOne, foodFour);
+// openFridge(foodOne, foodFour);
+
+const foods = getFood(foodOne, foodTwo, foodThree, foodFour, foodFive);
+
+// rest parameter -> bundle item into array
+console.log(foods); // Output -> [ 'Chicken Biriyani', 'Kacchi', 'Khichuri', 'Pizza', 'Fried Rice' ]
+
+// Example 2
