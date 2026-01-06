611bc78ec0004778ebc6ffaa794754d1f9a54f38
Sumit-531 <sumitchakraborty218@gmail.com>
2026-01-06 23:23:19 +0600

    add filter method example 2

--- 22. Advanced Javascript and DOM Manipulation/Javascript/Filter method/index.js
+++ 22. Advanced Javascript and DOM Manipulation/Javascript/Filter method/index.js
@@ -18,3 +18,21 @@ function isOdd(element) {
 
 console.log(evenNums); // Output -> [ 2, 4, 6 ]
 console.log(oddNums); // Output -> [ 1, 3, 5, 7 ]
+
+// Example 2
+
+const ages = [11, 12, 13, 13, 18, 19, 33, 36, 82];
+
+const adults = ages.filter(isAdult);
+const child = ages.filter(isChild);
+
+function isAdult(element) {
+  return element >= 18;
+}
+
+function isChild(element) {
+  return element < 18;
+}
+
+console.log(adults); // Output -> [ 18, 19, 33, 36, 82 ]
+console.log(child); // Output -> [ 11, 12, 13, 13 ]
