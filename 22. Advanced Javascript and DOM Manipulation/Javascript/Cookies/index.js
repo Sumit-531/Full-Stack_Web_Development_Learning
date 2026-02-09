deaa930b6ca8f9e6257fa451c69e7e6c54875572
Sumit-531 <sumitchakraborty218@gmail.com>
2026-02-09 22:54:02 +0600

    add Cookies

---
+++ 22. Advanced Javascript and DOM Manipulation/Javascript/Cookies/index.js
@@ -0,0 +1,10 @@
+// Cookie = a small text file stored in the computer
+//          used to rember information about the user
+//          saved in name=value pairs
+
+document.cookie =
+  "firstName=Sumit; expires=Wed, 2 February 2027 12:00:00 UTC; path=/";
+document.cookie =
+  "lastName=Chakraborty; expires=Wed, 19 February 2027 12:00:00 UTC; path=/";
+
+console.log(document.cookie);
